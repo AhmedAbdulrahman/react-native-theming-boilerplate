fedb79a461ef125e874d5cd4a213b73c1a6d1960
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-01 09:32:44 +0200

    :wrench: [.eslintrc] use constants for eslint modes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+const OFF = 0
+const WARNING = 1
+const ERROR = 2
+
 module.exports = {
   root: true, // So parent files don't get applied
   env: {
@@ -46,36 +50,36 @@ module.exports = {
    * their own groups.
    */
   rules: {
-    'arrow-body-style': 'off', // Don't enforce, readability firsthand.
-    'consistent-this': ['error', 'self'],
-    'linebreak-style': 'off', // Doesn't play nicely with Windows
-    'no-use-before-define': 'off',
+    'arrow-body-style': OFF, // Don't enforce, readability firsthand.
+    'consistent-this': [ERROR, 'self'],
+    'linebreak-style': OFF, // Doesn't play nicely with Windows
+    'no-use-before-define': OFF,
     // just as bad as "max components per file"
-    'max-classes-per-file': 'off',
-    'no-alert': 'error',
+    'max-classes-per-file': OFF,
+    'no-alert': ERROR,
     // Strict, airbnb is using warn; allow warn and error for dev environments
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'no-constant-condition': 'error',
+    'no-console': [ERROR, { allow: ['warn', ERROR] }],
+    'no-constant-condition': ERROR,
     // Airbnb use error
-    'no-param-reassign': 'off',
-    'no-prototype-builtins': 'off',
-    'nonblock-statement-body-position': 'error',
+    'no-param-reassign': OFF,
+    'no-prototype-builtins': OFF,
+    'nonblock-statement-body-position': ERROR,
     // Airbnb restricts isNaN and isFinite which are necessary for IE 11
     // we have to be disciplined about the usage and ensure the Number type for its
     // arguments
-    'no-underscore-dangle': 'error',
-    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
-    'prefer-destructuring': 'off', // Destructuring harm grep potential.
+    'no-underscore-dangle': ERROR,
+    'prefer-arrow-callback': [ERROR, { allowNamedFunctions: true }],
+    'prefer-destructuring': OFF, // Destructuring harm grep potential.
 
     // This rule is great for raising people awareness of what a key is and how it works.
-    'react/no-array-index-key': 'off',
-    'react/destructuring-assignment': 'off',
+    'react/no-array-index-key': OFF,
+    'react/destructuring-assignment': OFF,
     // It's buggy
-    'react/forbid-prop-types': 'off',
-    'react/jsx-curly-brace-presence': 'off',
-    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }], // airbnb is using .jsx
+    'react/forbid-prop-types': OFF,
+    'react/jsx-curly-brace-presence': OFF,
+    'react/jsx-filename-extension': [ERROR, { extensions: ['.js'] }], // airbnb is using .jsx
     'react/jsx-handler-names': [
-      'error',
+      ERROR,
       {
         // airbnb is disabling this rule
         eventHandlerPrefix: 'handle',
@@ -83,36 +87,38 @@ module.exports = {
       },
     ],
     // not a good rule for components close to the DOM
-    'react/jsx-props-no-spreading': 'off',
-    'react/no-danger': 'error',
+    'react/jsx-props-no-spreading': OFF,
+    'react/no-danger': ERROR,
     // Strict, airbnb is using off
-    'react/no-direct-mutation-state': 'error',
-    'react/no-find-dom-node': 'off',
-    'react/no-multi-comp': 'off',
-    'react/style-prop-object': 'off',
-    'react/require-default-props': 'off',
-    'react/sort-prop-types': 'off',
+    'react/no-direct-mutation-state': ERROR,
+    'react/no-find-dom-node': OFF,
+    'react/no-multi-comp': OFF,
+    'react/style-prop-object': OFF,
+    'react/require-default-props': OFF,
+    'react/sort-prop-types': OFF,
     // This depends entirely on what you're doing. There's no universal pattern
-    'react/state-in-constructor': 'off',
+    'react/state-in-constructor': OFF,
     // stylistic opinion. For conditional assignment we want it outside, otherwise as static
-    'react/static-property-placement': 'off',
+    'react/static-property-placement': OFF,
 
-    'import/namespace': ['error', { allowComputed: true }],
-    'import/no-extraneous-dependencies': 'off', // It would be better to enable this rule.
+    'import/prefer-default-export': OFF,
+    'import/no-absolute-path': [ERROR, { esmodule: false, commonjs: false, amd: false }],
+    'import/namespace': [ERROR, { allowComputed: true }],
+    'import/no-extraneous-dependencies': OFF, // It would be better to enable this rule.
     'import/order': [
-      'error',
+      ERROR,
       {
         groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
         'newlines-between': 'never',
       },
     ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': ['error', { additionalHooks: 'useEnhancedEffect' }],
+    'react-hooks/rules-of-hooks': ERROR,
+    'react-hooks/exhaustive-deps': [ERROR, { additionalHooks: 'useEnhancedEffect' }],
 
-    'react-native/no-unused-styles': 1,
-    'react-native/split-platform-components': 2,
-    'react-native/no-inline-styles': 1,
-    'react-native/no-color-literals': 0,
-    'react-native/no-raw-text': 0,
+    'react-native/no-unused-styles': WARNING,
+    'react-native/split-platform-components': ERROR,
+    'react-native/no-inline-styles': WARNING,
+    'react-native/no-color-literals': OFF,
+    'react-native/no-raw-text': OFF,
   },
 }
