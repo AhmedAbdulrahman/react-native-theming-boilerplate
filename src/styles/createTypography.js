075f938de701df37b198b9cad43e9fb4c70ff1ad
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-02 23:35:22 +0200

    :wrench: [styles] update fontFamilyPrimary

--- src/styles/createTypography.js
+++ src/styles/createTypography.js
@@ -1,15 +1,20 @@
-import { deepmerge } from '@utils'
-
 const caseAllCaps = {
   textTransform: 'uppercase',
 }
 
 export default function createTypography(typographyOptions) {
   const {
-    fontFamilyPrimary = 'SFProText-Regular',
-    fontFamilySecondary = 'SFProText-Bold',
+    fontFamilyPrimary = {
+      regular: 'SFProText-Regular',
+      semibold: 'SFProText-Semibold',
+      bold: 'SFProText-Bold',
+    },
     fontSize = 14, // px
-
+    fontWeightLight = '300',
+    fontWeightRegular = '400',
+    fontWeightMedium = '500',
+    fontWeightSemibold = '600',
+    fontWeightBold = '700',
     // Apply the CSS properties to all the variants.
     allVariants,
     ...other
@@ -25,26 +30,30 @@ export default function createTypography(typographyOptions) {
   })
 
   const variants = {
-    h1: buildVariant(fontFamilySecondary, 80, 80, 0.02, caseAllCaps),
-    h2: buildVariant(fontFamilySecondary, 40, 40, 0.02, caseAllCaps),
-    h3: buildVariant(fontFamilySecondary, 36, 36, 0.02, caseAllCaps),
-    h4: buildVariant(fontFamilySecondary, 24, 24, 0.02, caseAllCaps),
-    h5: buildVariant(fontFamilySecondary, 18, 18, 0, caseAllCaps),
-    h6: buildVariant(fontFamilySecondary, 16, 18, 0, caseAllCaps),
-    subtitle1: buildVariant(fontFamilyPrimary, 16, 18, 0.02),
-    subtitle2: buildVariant(fontFamilyPrimary, 14, 14, 0.02),
-    body1: buildVariant(fontFamilyPrimary, 16, 18, 0.02),
-    body2: buildVariant(fontFamilyPrimary, 14, 14, 0.02),
-    button: buildVariant(fontFamilyPrimary, 14, 14, 0.07, caseAllCaps),
-    caption: buildVariant(fontFamilyPrimary, 12, 14, 0.04),
-    overline: buildVariant(fontFamilyPrimary, 10, 16, 0.15, caseAllCaps),
+    h1: buildVariant(fontFamilyPrimary.regular, 80, 80, 0.02, caseAllCaps),
+    h2: buildVariant(fontFamilyPrimary.regular, 40, 40, 0.02, caseAllCaps),
+    h3: buildVariant(fontFamilyPrimary.regular, 36, 36, 0.02, caseAllCaps),
+    h4: buildVariant(fontFamilyPrimary.regular, 24, 24, 0.02, caseAllCaps),
+    h5: buildVariant(fontFamilyPrimary.regular, 18, 18, 0, caseAllCaps),
+    h6: buildVariant(fontFamilyPrimary.regular, 16, 18, 0, caseAllCaps),
+    subtitle1: buildVariant(fontFamilyPrimary.semibold, 16, 18, 0.02),
+    subtitle2: buildVariant(fontFamilyPrimary.semibold, 14, 14, 0.02),
+    body1: buildVariant(fontFamilyPrimary.regular, 16, 18, 0.02),
+    body2: buildVariant(fontFamilyPrimary.regular, 14, 14, 0.02),
+    button: buildVariant(fontFamilyPrimary.bold, 14, 14, 0.07, caseAllCaps),
+    caption: buildVariant(fontFamilyPrimary.regular, 12, 14, 0.04),
+    overline: buildVariant(fontFamilyPrimary.bold, 10, 16, 0.15, caseAllCaps),
   }
 
   return {
     fontFamilyPrimary,
-    fontFamilySecondary,
     fontSize,
     fontFamily: fontFamilyPrimary,
+    fontWeightLight,
+    fontWeightRegular,
+    fontWeightMedium,
+    fontWeightSemibold,
+    fontWeightBold,
     ...variants,
   }
 }
