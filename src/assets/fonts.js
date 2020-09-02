992d8430a197baed1cc38a918c347e4cf310ed9b
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-02 23:34:24 +0200

    :bento: [styles] fix fonts path & add mappings for fontfamily

--- src/assets/fonts.js
+++ src/assets/fonts.js
@@ -1,7 +1,7 @@
 const fonts = {
-  'SFProText-Bold': require('@assets/fonts/SF-Pro-Text-Bold.otf'),
-  'SFProText-Semibold': require('@assets/fonts/SF-Pro-Text-Semibold.otf'),
-  'SFProText-Regular': require('@assets/fonts/SF-Pro-Text-Regular.otf'),
+  'SFProText-Bold': require('../../assets/fonts/SF-Pro-Text-Bold.otf'),
+  'SFProText-Semibold': require('../../assets/fonts/SF-Pro-Text-Semibold.otf'),
+  'SFProText-Regular': require('../../assets/fonts/SF-Pro-Text-Regular.otf'),
 }
 
 export default fonts
---
+++ src/styles/fonts.js
@@ -0,0 +1,7 @@
+const fontFamily = {
+  bold: 'SFProText-Bold',
+  semibold: 'SFProText-Semibold',
+  regular: 'SFProText-Regular',
+}
+
+export default fontFamily
