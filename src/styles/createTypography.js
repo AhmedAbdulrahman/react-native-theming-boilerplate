b316834c9c173831200a3781b09b8383176a556a
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-05 20:12:49 +0200

    :wrench: [styles] adds unit to lineheight, update variants

--- src/styles/createTypography.js
+++ src/styles/createTypography.js
@@ -23,26 +23,26 @@ export default function createTypography(typographyOptions) {
   const buildVariant = (typeFace, fontSize, lineHeight, letterSpacing, casing) => ({
     fontFamily: typeFace,
     fontSize,
-    lineHeight,
+    lineHeight: `${fontSize * lineHeight}px`,
     letterSpacing,
     ...casing,
     ...allVariants,
   })
 
   const variants = {
-    h1: buildVariant(fontFamilyPrimary.regular, 80, 80, 0.02, caseAllCaps),
-    h2: buildVariant(fontFamilyPrimary.regular, 40, 40, 0.02, caseAllCaps),
-    h3: buildVariant(fontFamilyPrimary.regular, 36, 36, 0.02, caseAllCaps),
-    h4: buildVariant(fontFamilyPrimary.regular, 24, 24, 0.02, caseAllCaps),
-    h5: buildVariant(fontFamilyPrimary.regular, 18, 18, 0, caseAllCaps),
-    h6: buildVariant(fontFamilyPrimary.regular, 16, 18, 0, caseAllCaps),
-    subtitle1: buildVariant(fontFamilyPrimary.semibold, 16, 18, 0.02),
-    subtitle2: buildVariant(fontFamilyPrimary.semibold, 14, 14, 0.02),
-    body1: buildVariant(fontFamilyPrimary.regular, 16, 18, 0.02),
-    body2: buildVariant(fontFamilyPrimary.regular, 14, 14, 0.02),
-    button: buildVariant(fontFamilyPrimary.bold, 14, 14, 0.07, caseAllCaps),
-    caption: buildVariant(fontFamilyPrimary.regular, 12, 14, 0.04),
-    overline: buildVariant(fontFamilyPrimary.bold, 10, 16, 0.15, caseAllCaps),
+    h1: buildVariant(fontFamilyPrimary.regular, 75, 1, -0.03, caseAllCaps),
+    h2: buildVariant(fontFamilyPrimary.regular, 46, 1.1, -0.03, caseAllCaps),
+    h3: buildVariant(fontFamilyPrimary.regular, 40, 1.1, -0.03, caseAllCaps),
+    h4: buildVariant(fontFamilyPrimary.regular, 30, 1.2, -0.03, caseAllCaps),
+    h5: buildVariant(fontFamilyPrimary.regular, 32, 1.1, -0.03, caseAllCaps),
+    h6: buildVariant(fontFamilyPrimary.regular, 24, 1.1, -0.03, caseAllCaps),
+    subtitle1: buildVariant(fontFamilyPrimary.semibold, 16, 1.3, -0.02),
+    subtitle2: buildVariant(fontFamilyPrimary.semibold, 14, 1.3, -0.01),
+    body1: buildVariant(fontFamilyPrimary.regular, 16, 1.3, -0.02),
+    body2: buildVariant(fontFamilyPrimary.regular, 14, 1.3, -0.01),
+    button: buildVariant(fontFamilyPrimary.bold, 16, 1.25, -0.03, caseAllCaps),
+    caption: buildVariant(fontFamilyPrimary.regular, 12, 1.4, -0.01),
+    overline: buildVariant(fontFamilyPrimary.bold, 12, 1.4, -0.01, caseAllCaps),
   }
 
   return {
