a7d672b14b18e7a0397886acdb5b1648f93c07da
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-13 12:12:42 +0200

    :zap: [navigation] Add Link module

---
+++ src/navigation/Link.js
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import { useNavigation } from '@react-navigation/native'
+import Typography from 'components/Typography'
+
+const Link = React.forwardRef(function Link(props, ref) {
+  const {
+    component: Component = Typography,
+    to,
+    onPress,
+    params = {},
+    accessibilityRole = 'link',
+    children,
+    ...other
+  } = props
+  const navigation = useNavigation()
+
+  const extraProps = {}
+
+  if (Component.type?.displayName === 'Typography') {
+    extraProps.underline = true
+  }
+
+  const handlePress = React.useCallback(
+    (e) => {
+      navigation.navigate(to, params)
+      if (onPress) {
+        onPress(e)
+      }
+    },
+    [navigation, onPress, params, to],
+  )
+
+  return (
+    <Component ref={ref} onPress={handlePress} accessibilityRole={accessibilityRole} {...other}>
+      {children}
+    </Component>
+  )
+})
+Link.propTypes = {
+  component: PropTypes.element,
+  to: PropTypes.string,
+  onPress: PropTypes.func,
+  params: PropTypes.object,
+  children: PropTypes.node,
+  accessibilityRole: PropTypes.string,
+}
+export default React.memo(Link)
