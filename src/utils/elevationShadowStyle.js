94861414e43bbb207ae444c78cc3ce550d6c883e
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-13 12:53:54 +0200

    :art: [utils] Add elevationShadowStyle helper

---
+++ src/utils/elevationShadowStyle.js
@@ -0,0 +1,9 @@
+export default function elevationShadowStyle(elevation) {
+  return {
+    elevation,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 0.5 * elevation },
+    shadowOpacity: 0.2,
+    shadowRadius: 0.8 * elevation,
+  }
+}
