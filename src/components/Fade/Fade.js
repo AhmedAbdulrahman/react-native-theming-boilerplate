fc9db0e9a1d2e3ae0aff446962ed96659cf008b2
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-13 12:04:04 +0200

    :zap: [Fade] Add Fade component for Media loading

---
+++ src/components/Fade/Fade.js
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import { Platform } from 'react-native'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Animated, { Easing } from 'react-native-reanimated'
+import { mix, useTransition } from 'react-native-redash'
+
+const Root = styled(Animated.View)({})
+
+// FIXME: this animation is running in the JS thread should ideally run on the UI thread
+// look at using Animated.Value and withTiming instead of state and useTiming
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const {
+    children,
+    in: inProp = false,
+    transitionConfig = {
+      duration: 400,
+      easing: Easing.inOut(Easing.ease),
+    },
+    shadowStyle,
+    ...other
+  } = props
+
+  const transition = useTransition(inProp, transitionConfig)
+  const opacity = mix(transition, 0, 1)
+
+  return (
+    <Root
+      ref={ref}
+      style={{
+        opacity,
+        ...Platform.select({
+          ios: {
+            ...shadowStyle,
+          },
+          android: {
+            elevation: 5,
+          },
+        }),
+      }}
+      {...other}
+    >
+      {children}
+    </Root>
+  )
+})
+Fade.propTypes = {
+  children: PropTypes.node,
+  in: PropTypes.bool,
+  transitionConfig: PropTypes.object,
+  shadowStyle: PropTypes.object,
+}
+
+export default React.memo(Fade)
