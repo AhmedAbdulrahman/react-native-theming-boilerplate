fb5c4159523b1387116c64e7116ea4a21e868999
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-13 12:13:09 +0200

    :zap: [navigation] Add Routes mapping

---
+++ src/navigation/Routes.js
@@ -0,0 +1,16 @@
+export const Routes = {
+  Home: 'Home',
+  Onboarding: 'Onboarding',
+  Welcome: 'Welcome',
+  SignIn: 'SignIn',
+  SignUp: 'SignUp',
+  ForgotPassword: 'ForgotPassword',
+  Product: 'Product',
+}
+
+export const TabRoutes = {
+  Home: 'Home',
+  Shop: 'Shop',
+  Cart: 'Cart',
+  Profile: 'Profile',
+}
