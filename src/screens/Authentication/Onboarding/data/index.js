3378393a44bc32e19749468164041d18eb4f5010
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-13 12:14:09 +0200

    :zap: [Onboarding] Add dumy data for sliders

---
+++ src/screens/Authentication/Onboarding/data/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable global-require */
+module.exports = [
+  {
+    title: 'All your favorites',
+    description: 'Order from the best local restaurants with easy, on-demand delivery.',
+    picture: require('../../../../../assets/images/favorite.png'),
+    color: '#DDF1E6',
+  },
+  {
+    title: 'Free delivery offers',
+    description: 'Free delivery for new customers via Apple Pay and others payment methods.',
+    picture: require('../../../../../assets/images/offers.png'),
+    color: '#F1E1DD',
+  },
+  {
+    title: 'Choose your food',
+    description: 'Easily find your type of food craving and you’ll get delivery in wide range.',
+    picture: require('../../../../../assets/images/pizza.png'),
+    color: '#F1EFDD',
+  },
+]
