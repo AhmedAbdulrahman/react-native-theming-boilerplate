3d15dd05c1984431a3358a1c0c2e922ad1144cd5
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-19 14:08:40 +0200

    :wrench: [Media] Adds support for animated component

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,33 +1,28 @@
 import * as React from 'react'
-import { Platform } from 'react-native'
+import PropTypes from 'prop-types'
+import { Animated } from 'react-native'
 import styled from 'styled-components'
 
 const BaseMedia = styled.Image((props) => {
-  const { theme, rounded, enableShadow } = props
+  const { theme, rounded } = props
 
   return {
     ...(rounded && { borderRadius: theme.spacing(1.2) }),
-    ...(enableShadow && {
-      ...Platform.select({
-        ios: {
-          shadowColor: '#000',
-          shadowOffset: { width: 0, height: 2 },
-          shadowOpacity: 0.8,
-          shadowRadius: 2,
-        },
-        android: {
-          elevation: 5,
-        },
-      }),
-    }),
   }
 })
 
 const Media = React.forwardRef(function Media(props, ref) {
-  const { uri, local, component = BaseMedia, ...other } = props
-  const Component = component
+  const { uri, local, component = BaseMedia, animated, ...other } = props
+  const Component = animated ? Animated.createAnimatedComponent(component) : component
 
   return <Component ref={ref} source={local ? uri : { uri }} {...other} />
 })
+
+Media.propTypes = {
+  animated: PropTypes.bool,
+  component: PropTypes.element,
+  local: PropTypes.bool,
+  uri: PropTypes.string,
+}
 
 export default React.memo(Media)
