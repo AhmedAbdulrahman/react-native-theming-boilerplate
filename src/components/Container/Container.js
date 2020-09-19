cc790ab744283437b09c3b93b7fb5322b4dfc36f
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-19 14:50:16 +0200

    :wrench: [Container] use mixins for padding

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,10 +2,10 @@ import styled from 'styled-components'
 import Constants from 'expo-constants'
 
 const Container = styled.View((props) => ({
-  backgroundColor: props.theme.palette.background.paper.string(),
+  ...props.theme.mixins.container,
+  backgroundColor: props.theme.palette.background.default.string(),
   flex: 1,
-  paddingHorizontal: props.theme.spacing(1),
-  paddingTop: Constants.statusBarHeight,
+  paddingTop: Constants.statusBarHeight * 0.1,
 }))
 
 export default Container
