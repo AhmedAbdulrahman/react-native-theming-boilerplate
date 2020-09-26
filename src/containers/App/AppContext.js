d64c87774bb02bb3ddea9acad032e1215af6d5f6
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-26 15:09:01 +0200

    :wrench: [AppContext] add useAppHandlers context for local handlers

--- src/containers/App/AppContext.js
+++ src/containers/App/AppContext.js
@@ -1,18 +1,35 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
 
 const AppContext = React.createContext({})
+const AppHandlers = React.createContext({})
 
 const AppProvider = (props) => {
   const { children } = props
   const [isLoading, setIsLoading] = React.useState(true)
 
-  const context = React.useMemo(() => ({ isLoading, setIsLoading }), [])
+  const contextHandlers = React.useMemo(() => ({ setIsLoading }), [setIsLoading])
+  const context = React.useMemo(() => ({ ...contextHandlers, isLoading }), [
+    contextHandlers,
+    isLoading,
+  ])
 
-  return <AppContext.Provider value={context}>{children}</AppContext.Provider>
+  return (
+    <AppContext.Provider value={context}>
+      <AppHandlers.Provider value={contextHandlers}>{children}</AppHandlers.Provider>
+    </AppContext.Provider>
+  )
 }
 
 export const useApp = () => {
   return React.useContext(AppContext)
 }
 
+export const useAppHandlers = () => {
+  return React.useContext(AppHandlers)
+}
+
+AppProvider.propTypes = {
+  children: PropTypes.node,
+}
 export default AppProvider
