5f1b5ffba0553f8fafa5b7446421472f43ce9692
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-09-26 13:41:59 +0200

    :zap::wrench: [ProductFlatList][HeroItem] Move Hero list to its own component, and update ProductFlatList

--- src/containers/ProductFlatList/ProductFlatList.js
+++ src/containers/ProductFlatList/ProductFlatList.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 import { Animated, StyleSheet, Platform } from 'react-native'
 import { useTheme } from 'styled-components'
 import ProductCard from 'containers/ProductCard'
+import Spacing from 'components/Spacing'
+import HeroItem from './partials/HeroItem'
 
 const ProductFlatList = (props) => {
-  const { children, section, ...other } = props
+  const { children, section, horizontal, ...other } = props
   const theme = useTheme()
 
   const ITEM_SIZE =
@@ -15,7 +17,7 @@ const ProductFlatList = (props) => {
   const FULLSIZE = ITEM_SIZE + theme.spacing() * 2
   const IMAGE_SIZE = theme.extras.constants.WINDOW_WIDTH * 0.4
 
-  const ITEM_FULLSIZE = IMAGE_SIZE + theme.spacing() * 2
+  const ITEM_FULLSIZE = IMAGE_SIZE * 0.5 + theme.spacing() * 2
 
   const scrollX = React.useRef(new Animated.Value(0)).current
 
@@ -33,46 +35,9 @@ const ProductFlatList = (props) => {
         outputRange: [0.5, 1, 0.5],
       })
 
-      return (
-        <ProductCard
-          product={item}
-          style={{
-            // width: ITEM_SIZE,
-            // height: ITEM_SIZE * 0.5,
-            marginVertical: theme.spacing(),
-            marginLeft: theme.spacing(),
-            marginRight: theme.spacing(),
-          }}
-          type="hero"
-          MediaContaierProps={{
-            style: [
-              {
-                height: ITEM_SIZE * 0.6,
-                width: ITEM_SIZE,
-                overflow: 'hidden',
-                borderRadius: theme.spacing(1.2),
-              },
-            ],
-          }}
-          MediaProps={{
-            animated: true,
-            rounded: true,
-            style: [
-              //   styles.heroMedia,
-              StyleSheet.absoluteFillObject,
-              {
-                opacity,
-                transform: [{ scale }],
-                width: ITEM_SIZE,
-                height: ITEM_SIZE,
-                resizeMode: 'cover',
-              },
-            ],
-          }}
-        />
-      )
+      return <HeroItem item={item} opacity={opacity} scale={scale} itemSize={ITEM_SIZE} />
     },
-    [FULLSIZE, ITEM_SIZE, scrollX, theme],
+    [FULLSIZE, ITEM_SIZE, scrollX],
   )
 
   const renderListItem = React.useCallback(
@@ -81,50 +46,51 @@ const ProductFlatList = (props) => {
 
       const scale = scrollX.interpolate({
         inputRange,
-        outputRange: [1.2, 1.3, 1.2],
+        outputRange: [1.3, 1, 1.1],
       })
 
       return (
-        <ProductCard
-          type="normal"
-          style={{
-            margin: theme.spacing(),
-          }}
-          product={item}
-          MediaContaierProps={{
-            style: [
-              {
-                height: ITEM_SIZE * 0.5,
-                width: ITEM_SIZE * 0.72,
-                overflow: 'hidden',
-                borderRadius: theme.spacing(1.2),
-              },
-            ],
-          }}
-          MediaProps={{
-            animated: true,
-            rounded: true,
-            style: [
-              //   styles.normalItemMedia,
-              StyleSheet.absoluteFillObject,
-              {
-                transform: [{ scale }],
-                width: ITEM_FULLSIZE * 1.3,
-                height: IMAGE_SIZE * 0.8,
-                resizeMode: 'cover',
-              },
-            ],
-          }}
-        />
+        <Spacing key={item} mb={3}>
+          <ProductCard
+            type="normal"
+            style={{
+              marginVertical: theme.spacing(),
+              marginLeft: theme.spacing(),
+              marginRight: theme.spacing(),
+            }}
+            product={item}
+            MediaContaierProps={{
+              style: [
+                {
+                  height: ITEM_FULLSIZE,
+                  width: IMAGE_SIZE * 1.2,
+                  overflow: 'hidden',
+                  borderRadius: theme.spacing(1.2),
+                },
+              ],
+            }}
+            MediaProps={{
+              animated: true,
+              rounded: true,
+              style: [
+                StyleSheet.absoluteFillObject,
+                {
+                  transform: [{ scale }],
+                  resizeMode: 'cover',
+                },
+              ],
+            }}
+          />
+        </Spacing>
       )
     },
-    [FULLSIZE, IMAGE_SIZE, scrollX, theme],
+    [FULLSIZE, IMAGE_SIZE, ITEM_FULLSIZE, scrollX, theme],
   )
 
   return (
     <Animated.FlatList
       data={section.sectionContent}
-      horizontal
+      horizontal={horizontal}
       renderToHardwareTextureAndroid
       bounces={false}
       snapToAlignment="center"
@@ -145,6 +111,7 @@ const ProductFlatList = (props) => {
 ProductFlatList.propTypes = {
   children: PropTypes.node,
   section: PropTypes.object,
+  horizontal: PropTypes.bool,
 }
 
 export default ProductFlatList
---
+++ src/containers/ProductFlatList/partials/HeroItem.js
@@ -0,0 +1,56 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet } from 'react-native'
+import { useTheme } from 'styled-components'
+import ProductCard from 'containers/ProductCard'
+import Spacing from 'components/Spacing'
+
+const HeroItem = (props) => {
+  const { item, itemSize, opacity, scale, ...other } = props
+
+  const theme = useTheme()
+
+  return (
+    <Spacing key={item} mb={3}>
+      <ProductCard
+        product={item}
+        style={{
+          marginVertical: theme.spacing(),
+          marginLeft: theme.spacing(),
+          marginRight: theme.spacing(),
+        }}
+        type="hero"
+        MediaContaierProps={{
+          style: {
+            width: itemSize,
+            height: itemSize * 0.6,
+            overflow: 'hidden',
+            borderRadius: theme.spacing(1.2),
+          },
+        }}
+        MediaProps={{
+          animated: true,
+          rounded: true,
+          style: [
+            StyleSheet.absoluteFillObject,
+            {
+              opacity,
+              transform: [{ scale }],
+              resizeMode: 'cover',
+            },
+          ],
+        }}
+        {...other}
+      />
+    </Spacing>
+  )
+}
+
+HeroItem.propTypes = {
+  item: PropTypes.object,
+  itemSize: PropTypes.number,
+  opacity: PropTypes.object,
+  scale: PropTypes.object,
+}
+
+export default React.memo(HeroItem)
