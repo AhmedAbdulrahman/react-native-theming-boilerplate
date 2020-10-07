13102257b51884e664cb3b0d81d819570015edc2
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-10-07 23:47:10 +0200

    :zap: [utils] add helpers for loading & saving to localStorage

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 export { default as getContrastRatio } from './getContrastRatio'
 export { default as elevationShadowStyle } from './elevationShadowStyle'
+export { loadSettings, saveSettings } from './storage'
---
+++ src/utils/storage.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import AsyncStorage from '@react-native-community/async-storage'
+
+const STORAGE_KEY = 'SETTINGS'
+
+const DEFAULT_SETTINGS = {
+  dark: false,
+}
+
+export const loadSettings = async () => {
+  try {
+    const settings = await AsyncStorage.getItem(STORAGE_KEY)
+    return settings != null ? JSON.parse(settings) : DEFAULT_SETTINGS
+  } catch (error) {
+    console.log('Error loading settings', error)
+    return null
+  }
+}
+
+export const saveSettings = async (settings) => {
+  try {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(settings))
+  } catch (error) {
+    console.log('Error loading settings', error)
+  }
+}
