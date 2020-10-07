3fc4346c09a00ca2088381b28d412cbe188cb2fb
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-10-07 23:48:26 +0200

    :wrench: [AppContext] handle theme toggle feature

--- src/containers/App/AppContext.js
+++ src/containers/App/AppContext.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
+import { loadSettings, saveSettings } from 'utils'
 
 const AppContext = React.createContext({})
 const AppHandlers = React.createContext({})
@@ -7,11 +8,42 @@ const AppHandlers = React.createContext({})
 const AppProvider = (props) => {
   const { children } = props
   const [isLoading, setIsLoading] = React.useState(true)
+  const [currentTheme, setCurrentTheme] = React.useState('light')
 
-  const contextHandlers = React.useMemo(() => ({ setIsLoading }), [setIsLoading])
-  const context = React.useMemo(() => ({ ...contextHandlers, isLoading }), [
+  const getSettings = React.useCallback(async () => {
+    const localTheme = await loadSettings()
+
+    if (localTheme) {
+      setCurrentTheme(localTheme)
+    } else {
+      saveSettings({ theme: 'light' })
+    }
+  }, [])
+
+  const onThemeToggle = React.useCallback(async () => {
+    if (currentTheme === 'light') {
+      setCurrentTheme('dark')
+      saveSettings('dark')
+    } else {
+      setCurrentTheme('light')
+      saveSettings('light')
+    }
+  }, [currentTheme])
+
+  // Side effects
+  React.useEffect(() => {
+    getSettings()
+  })
+
+  const contextHandlers = React.useMemo(() => ({ onThemeToggle, setIsLoading }), [
+    onThemeToggle,
+    setIsLoading,
+  ])
+
+  const context = React.useMemo(() => ({ ...contextHandlers, isLoading, currentTheme }), [
     contextHandlers,
     isLoading,
+    currentTheme,
   ])
 
   return (
