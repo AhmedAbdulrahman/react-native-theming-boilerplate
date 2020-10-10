e98887ade4e2aacf8ad46c4c3db98f6f9c43c39d
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-10-11 00:22:55 +0200

    :wrench: [navigation] Move TabNavigation into main stacks screens

--- src/navigation/TabNavigation/index.js
+++
@@ -1,2 +0,0 @@
-export {default} from './TabNavigation';
-export {Tab} from './TabNavigation';
--- src/navigation/stacks/index.js
+++ src/navigation/stacks/index.js
@@ -1,6 +1,77 @@
-export { default as AppStack } from './AppStack'
-export { default as AuthenticationStack } from './AuthenticationStack'
-export { default as HomeStack } from './HomeStack'
-export { default as ProfileStack } from './ProfileStack'
-export { default as OrdersStack } from './OrdersStack'
-export { default as ShopStack } from './ShopStack'
+import * as React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { useTheme } from 'styled-components'
+import { TabRoutes } from 'navigation/Routes'
+import TabBarIcon from './partials/TabBarIcon'
+import HomeStack from './HomeStack'
+import ProfileStack from './ProfileStack'
+import ShopStack from './ShopStack'
+import OrdersStack from './OrdersStack'
+import AuthenticationStack from './AuthenticationStack'
+
+const AppStack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const AppStackScreen = () => {
+  const theme = useTheme()
+  return (
+    <AppStack.Navigator headerMode="none">
+      <AppStack.Screen name="Authentication" component={AuthenticationStack} />
+      <AppStack.Screen name="Home">
+        {() => (
+          <Tab.Navigator
+            tabBarOptions={{
+              activeTintColor: theme.palette.success.main.string(),
+              inactiveTintColor: theme.palette.text.disabled.string(),
+              style: {
+                paddingHorizontal: theme.spacing(4),
+                height: 85,
+                backgroundColor: theme.palette.background.default.string(),
+              },
+              labelStyle: {
+                fontSize: 12,
+              },
+            }}
+          >
+            <Tab.Screen
+              key={TabRoutes.Home}
+              name={TabRoutes.Home}
+              component={HomeStack}
+              options={{
+                tabBarIcon: (tabBarIconProps) => <TabBarIcon icon="Food" {...tabBarIconProps} />,
+              }}
+            />
+            <Tab.Screen
+              key={TabRoutes.Search}
+              name={TabRoutes.Search}
+              component={ShopStack}
+              options={{
+                tabBarIcon: (tabBarIconProps) => <TabBarIcon icon="Search" {...tabBarIconProps} />,
+              }}
+            />
+            <Tab.Screen
+              key={TabRoutes.Orders}
+              name={TabRoutes.Orders}
+              component={OrdersStack}
+              options={{
+                tabBarIcon: (tabBarIconProps) => <TabBarIcon icon="Order" {...tabBarIconProps} />,
+                tabBarBadge: 3,
+              }}
+            />
+            <Tab.Screen
+              key={TabRoutes.Profile}
+              name={TabRoutes.Profile}
+              component={ProfileStack}
+              options={{
+                tabBarIcon: (tabBarIconProps) => <TabBarIcon icon="Profile" {...tabBarIconProps} />,
+              }}
+            />
+          </Tab.Navigator>
+        )}
+      </AppStack.Screen>
+    </AppStack.Navigator>
+  )
+}
+
+export default AppStackScreen
