1bc32d6cb303540eaf5597819f48a26dbb712194
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-04-24 17:59:33 +0200

    :wrench: [navigation][HomeStack] remove screenOption Header styling

--- src/navigation/stacks/HomeStack.js
+++ src/navigation/stacks/HomeStack.js
@@ -1,40 +1,19 @@
 import * as React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import { useTheme } from 'styled-components'
-import { useAppHandlers, useApp } from 'containers/App/AppContext'
 import HomeScreen from 'screens/Home'
-import Button from 'components/Button'
 import ProductScreen from 'screens/Product'
 import { Routes } from 'navigation/Routes'
 
 const HomeStack = createStackNavigator()
 
 const HomeStackScreen = () => {
-  const { onThemeToggle } = useAppHandlers()
-  const { currentTheme } = useApp()
-
-  const theme = useTheme()
-
   return (
-    <HomeStack.Navigator
-      screenOptions={{
-        headerTitleStyle: {
-          color: theme.palette.text.primary.rgb().string(),
-        },
-        headerStyle: {
-          backgroundColor: theme.palette.background.default.rgb().string(),
-          borderBottomWidth: 0,
-        },
-        headerTintColor: 'white',
-        headerLeft: false,
-        headerRight: () => (
-          <Button variant="text" disableUppercase onPress={onThemeToggle}>
-            Filters
-          </Button>
-        ),
-      }}
-    >
-      <HomeStack.Screen name={Routes.Home} component={HomeScreen} />
+    <HomeStack.Navigator screenOptions={{ gestureEnabled: false }}>
+      <HomeStack.Screen
+        options={{ headerShown: false }}
+        name={Routes.Home}
+        component={HomeScreen}
+      />
       <HomeStack.Screen
         name={Routes.Product}
         component={ProductScreen}
