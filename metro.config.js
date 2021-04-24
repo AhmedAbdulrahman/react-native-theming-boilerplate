49f723d0766b2151a53b8feaaf189f3a0b694965
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-04-25 01:52:00 +0200

    :package: [npm][config] Customizing metro, add font otf extension

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,17 @@
-const { getDefaultConfig } = require('metro-config')
+const { getDefaultConfig } = require('@expo/metro-config')
 
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig()
+  } = await getDefaultConfig(__dirname)
+  console.log('sourceExts', [...sourceExts, 'otf', 'svg'])
   return {
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
+      sourceExts: [...sourceExts, 'otf', 'svg'],
     },
   }
 })()
