fdda886e63711cf022a2c3d3fd002275f604c7ac
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-05-03 17:40:16 +0200

    :package::wrench: [Media] refactor Media component - Replace Image with AutoHeightImage component - Use responsive dimensions for fonts, heights and widths

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,28 +1,77 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import { Animated } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import styled from 'styled-components'
+import { useResponsiveWidth } from 'react-native-responsive-dimensions'
+import Image from 'react-native-auto-height-image'
 
-const BaseMedia = styled.Image((props) => {
-  const { theme, rounded } = props
-
+const Root = styled(View)((props) => {
+  const { theme, rounded = true } = props
   return {
     ...(rounded && { borderRadius: theme.spacing(1.2) }),
+    overflow: 'hidden',
+  }
+})
+
+const BaseMedia = styled(Image)((props) => {
+  const { width } = props
+  return {
+    flexGrow: 1,
+    width,
+  }
+})
+
+const Overlay = styled(View)((props) => {
+  const { backgroundColor = 'black', opacity = 0.3 } = props
+  return {
+    ...StyleSheet.absoluteFillObject,
+    opacity,
+    backgroundColor,
   }
 })
 
 const Media = React.forwardRef(function Media(props, ref) {
-  const { uri, local, component = BaseMedia, animated, ...other } = props
-  const Component = animated ? Animated.createAnimatedComponent(component) : component
+  const {
+    uri,
+    local,
+    withOverlay,
+    overlayProps = {},
+    MediaProps: {
+      width: MediaPropsWidth = 45,
+      height: MediaPropsHeight = 45,
+      ...MediaPropsProp
+    } = {},
+    onLoad,
+    ...other
+  } = props
 
-  return <Component ref={ref} source={local ? uri : { uri }} {...other} />
+  const width = useResponsiveWidth(MediaPropsWidth)
+  const height = useResponsiveWidth(MediaPropsHeight)
+
+  return (
+    <Root ref={ref} {...other}>
+      <BaseMedia
+        source={local ? uri : { uri }}
+        width={width}
+        height={height}
+        onLoad={onLoad}
+        {...MediaPropsProp}
+      />
+      {withOverlay && <Overlay {...overlayProps} />}
+    </Root>
+  )
 })
 
 Media.propTypes = {
   animated: PropTypes.bool,
   component: PropTypes.element,
   local: PropTypes.bool,
+  MediaProps: PropTypes.object,
+  onLoad: PropTypes.func,
+  overlayProps: PropTypes.object,
   uri: PropTypes.string,
+  width: PropTypes.number,
+  withOverlay: PropTypes.bool,
 }
 
 export default React.memo(Media)
