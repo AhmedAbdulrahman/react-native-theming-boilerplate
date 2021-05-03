6ccfec5c8c99de7225e4fd59cca5deafc6e0f078
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-05-03 19:25:25 +0200

    :wrench: [screens/Product] house keeping & remove ProductToolbar from header

--- src/screens/Product/partials/ProductHeader.js
+++ src/screens/Product/partials/ProductHeader.js
@@ -2,8 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { View } from 'react-native'
 import { useTheme } from 'styled-components'
-import ProductToolbar from 'containers/ProductToolbar'
-import Flex from 'components/Flex'
 import Spacing from 'components/Spacing'
 import Typography from 'components/Typography'
 import SvgIcon from 'components/SvgIcon'
@@ -11,82 +9,83 @@ import Button from 'components/Button'
 
 export const MIN_HEADER_HEIGHT = 150
 
-const ProductHeader = ({ product }) => {
+const ProductHeader = ({ resturant }) => {
   const theme = useTheme()
   return (
     <Spacing mb={3} mt={1}>
       <View>
-        <ProductToolbar isProductScreen {...product} />
-        <Spacing mb={2} mt={1}>
-          {/* eslint-disable-next-line react-native/no-inline-styles */}
-          <Flex flexDirection="row" align="center" justify="space-between" style={{ width: 150 }}>
-            <Typography color="grey" variant="body2">
+        <Spacing container mt={3}>
+          <Typography color="dark" variant="h3">
+            {resturant.title}
+          </Typography>
+          <Typography color="textSecondary" variant="subtitle" gutterTop>
+            {resturant.types?.join(' • ')}
+          </Typography>
+          <Spacing container mt={2} mb={2} fd="row" ai="center">
+            <Typography color="grey" variant="body1">
               4.3
             </Typography>
-            <SvgIcon icon="Star" color={theme.palette.success.main.string()} />
-            <Typography color="grey" variant="body2">
+            <Spacing mr={1} ml={1}>
+              <SvgIcon icon="Star" color={theme.palette.success.main.string()} />
+            </Spacing>
+            <Typography color="grey" variant="body1">
               200+ Ratings
             </Typography>
-          </Flex>
+          </Spacing>
         </Spacing>
-        <Spacing mt={2}>
-          <Flex flexDirection="row" justify="space-between">
-            <Flex flexDirection="row">
-              <Flex flexDirection="row">
-                <Spacing mr={1}>
-                  <SvgIcon
-                    icon="Active"
-                    width={24}
-                    height={28}
-                    color={theme.palette.success.main.string()}
-                    // eslint-disable-next-line react-native/no-inline-styles
-                    style={{
-                      position: 'absolute',
-                      top: -5,
-                      left: -8,
-                    }}
-                  />
-                  <SvgIcon icon="Currency" color={theme.palette.success.main.string()} />
-                </Spacing>
-                <Flex>
-                  <Typography color="dark" variant="subtitle2">
-                    Free
-                  </Typography>
-                  <Typography color="textSecondary" variant="body2">
-                    Delivery
-                  </Typography>
-                </Flex>
-              </Flex>
-              <Flex flexDirection="row">
-                <Spacing mr={1} ml={-2}>
-                  <SvgIcon
-                    icon="Active"
-                    width={24}
-                    height={28}
-                    color={theme.palette.success.main.string()}
-                    // eslint-disable-next-line react-native/no-inline-styles
-                    style={{
-                      position: 'absolute',
-                      top: -5,
-                      left: -5,
-                    }}
-                  />
-                  <SvgIcon icon="Time" color={theme.palette.success.main.string()} />
-                </Spacing>
-                <Flex>
-                  <Typography color="dark" variant="subtitle2">
-                    25
-                  </Typography>
-                  <Typography color="textSecondary" variant="body2">
-                    Minutes
-                  </Typography>
-                </Flex>
-              </Flex>
-            </Flex>
-            <Button size="large" color="success" variant="outlined">
-              Take away
-            </Button>
-          </Flex>
+
+        <Spacing container mt={2} fd="row" jc="space-between" ai="center">
+          <Spacing container fd="row">
+            <Spacing container mr={2} fd="row">
+              <SvgIcon
+                icon="Active"
+                width={24}
+                height={28}
+                color={theme.palette.success.main.string()}
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={{
+                  position: 'absolute',
+                  top: -5,
+                  left: -8,
+                }}
+              />
+              <SvgIcon icon="Currency" color={theme.palette.success.main.string()} />
+              <Spacing container ml={1}>
+                <Typography color="dark" variant="subtitle2">
+                  Free
+                </Typography>
+                <Typography color="textSecondary" variant="body2">
+                  Delivery
+                </Typography>
+              </Spacing>
+            </Spacing>
+            <Spacing container fd="row">
+              <SvgIcon
+                icon="Active"
+                width={24}
+                height={28}
+                color={theme.palette.success.main.string()}
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={{
+                  position: 'absolute',
+                  top: -5,
+                  left: -8,
+                }}
+              />
+              <SvgIcon icon="Time" color={theme.palette.success.main.string()} />
+              <Spacing container ml={1}>
+                <Typography color="dark" variant="subtitle2">
+                  25
+                </Typography>
+                <Typography color="textSecondary" variant="body2">
+                  Minutes
+                </Typography>
+              </Spacing>
+            </Spacing>
+          </Spacing>
+          <Button size="medium" color="success" variant="outlined" borderRadius={2}>
+            Take away
+          </Button>
         </Spacing>
       </View>
     </Spacing>
@@ -94,7 +93,7 @@ const ProductHeader = ({ product }) => {
 }
 
 ProductHeader.propTypes = {
-  product: PropTypes.object,
+  resturant: PropTypes.object,
 }
 
 export default ProductHeader
