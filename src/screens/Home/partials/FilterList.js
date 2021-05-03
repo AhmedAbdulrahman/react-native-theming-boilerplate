60f32e02ffd53e6f9b8abc938921181b193ece9c
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-05-03 19:21:29 +0200

    :wrench: [screens] cleanup FilterList

--- src/screens/Home/partials/FilterList.js
+++ src/screens/Home/partials/FilterList.js
@@ -1,10 +1,8 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { TouchableOpacity } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Button from 'components/Button'
 import Container from 'components/Container'
-import Flex from 'components/Flex'
 import Header from 'components/Header'
 import IconButton from 'components/IconButton'
 import Modal from 'components/Modal'
@@ -21,7 +19,6 @@ const FilterList = React.forwardRef(function FilterList(props, ref) {
     ...other
   } = props
 
-  const insets = useSafeAreaInsets()
   const isFilterActive =
     filterOption.categories.length > 0 ||
     filterOption.dietary.length > 0 ||
@@ -37,72 +34,69 @@ const FilterList = React.forwardRef(function FilterList(props, ref) {
         }
         centerComponent={<Typography variant="h6">Filters</Typography>}
       />
-      <Container>
-        <Flex>
-          {filters.map((filter, idx) => {
-            const { heading, name } = filter
+      <Container flex={1}>
+        {filters.map((filter, idx) => {
+          const { heading, name } = filter
 
-            const value = filterOption[name] || []
+          const value = filterOption[name] || []
 
-            return (
-              <Spacing container key={idx}>
-                <Spacing container mt={1} mb={2} fd="row" jc="space-between" ai="center">
-                  <Typography variant="h6" uppercase>
-                    {heading}
-                  </Typography>
+          return (
+            <Spacing container key={idx} mb={5}>
+              <Spacing container mt={1} mb={1} fd="row" jc="space-between" ai="center">
+                <Typography color="dark" variant="h6" uppercase>
+                  {heading}
+                </Typography>
 
-                  <Typography
-                    variant="body2"
-                    uppercase
-                    component={Button}
-                    onPress={() => handleResetClick(name)}
-                  >
-                    Clear All
-                  </Typography>
-                </Spacing>
-
-                <Spacing container mr={2} mb={3} fd="row" fw="wrap">
-                  {filter.options?.map((option) => {
-                    const selected = value.includes(option.key)
-
-                    return (
-                      <Spacing key={option.id} mr={1} mb={2}>
-                        <Button
-                          size="large"
-                          circle={name === 'price'}
-                          color={selected ? 'success' : 'default'}
-                          variant="contained"
-                          onPress={() => {
-                            handleFilterChange(name, option.key)
-                          }}
-                        >
-                          <Typography
-                            color={selected ? 'textWhite' : 'textSecondary'}
-                            variant="body1"
-                          >
-                            {option.label}
-                          </Typography>
-                        </Button>
-                      </Spacing>
-                    )
-                  })}
-                </Spacing>
+                <Typography
+                  color="dark"
+                  variant="body2"
+                  uppercase
+                  component={Button}
+                  onPress={() => handleResetClick(name)}
+                >
+                  Clear All
+                </Typography>
               </Spacing>
-            )
-          })}
-          <Button
-            enabled={isFilterActive}
-            disabled={!isFilterActive}
-            size="large"
-            fullWidth
-            color="success"
-            // eslint-disable-next-line react-native/no-inline-styles
-            style={{ position: 'absolute', bottom: insets.bottom }}
-            onPress={handleModalHide}
-          >
-            Apply filters
-          </Button>
-        </Flex>
+
+              <Spacing container mr={2} fd="row" fw="wrap">
+                {filter.options?.map((option) => {
+                  const selected = value.includes(option.key)
+
+                  return (
+                    <Spacing key={option.id} mr={1} mb={2}>
+                      <Button
+                        size="large"
+                        circle={name === 'price'}
+                        color={selected ? 'success' : 'default'}
+                        variant="contained"
+                        onPress={() => {
+                          handleFilterChange(name, option.key)
+                        }}
+                      >
+                        <Typography
+                          color={selected ? 'textWhite' : 'textSecondary'}
+                          variant="body1"
+                        >
+                          {option.label}
+                        </Typography>
+                      </Button>
+                    </Spacing>
+                  )
+                })}
+              </Spacing>
+            </Spacing>
+          )
+        })}
+        <Button
+          enabled={isFilterActive}
+          disabled={!isFilterActive}
+          size="large"
+          fullWidth
+          color="success"
+          onPress={handleModalHide}
+        >
+          Apply filters
+        </Button>
       </Container>
     </Modal>
   )
