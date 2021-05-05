927f16af9fe34deefd77bd26353e31a8a1bd0ee8
Ahmed Abdulrahman <a.kasapbashi@live.com>
2021-05-05 14:47:23 +0200

    :wrench: [ProductToolbar] cleanup unused stuff

--- src/containers/ProductToolbar/ProductToolbar.js
+++ src/containers/ProductToolbar/ProductToolbar.js
@@ -1,74 +1,86 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import Flex from 'components/Flex'
+import styled, { useTheme } from 'styled-components'
 import Typography from 'components/Typography'
 import Spacing from 'components/Spacing'
+import SvgIcon from 'components/SvgIcon'
 
-const Root = styled(Flex)((props) => ({
-  paddingTop: props.theme.spacing(2),
-}))
-
-const Rating = styled.View((props) => ({
-  backgroundColor: props.theme.palette.success.main.string(),
-  paddingVertical: props.theme.spacing(0.3),
-  paddingHorizontal: props.theme.spacing(1.3),
-  borderRadius: props.theme.spacing(1),
-  marginRight: props.theme.spacing(2),
-}))
+const Rating = styled.View((props) => {
+  const { theme, transparent } = props
+  return {
+    ...(!transparent && { backgroundColor: theme.palette.success.main.string() }),
+    paddingVertical: theme.spacing(0.3),
+    paddingHorizontal: theme.spacing(1.3),
+    borderRadius: theme.spacing(1),
+  }
+})
 
 const ProductToolbar = (props) => {
-  const { title, address, types, type, isProductScreen, ...other } = props
-
+  const { title, types, horizontal } = props
+  const theme = useTheme()
   return (
-    <Root {...other}>
-      <Typography color="dark" variant={isProductScreen ? 'h5' : 'h6'}>
+    <Spacing container mt={2}>
+      <Typography color="dark" variant="subhead">
         {title}
       </Typography>
+      <Typography color="textSecondary" variant={!horizontal ? 'subtitle' : 'body1'} gutterTop>
+        {types?.join(' • ')}
+      </Typography>
 
-      <Spacing mb={1}>
-        {type === 'meal' ? (
-          <Typography color="textSecondary" variant="body1" gutterTop>
-            {address}
-          </Typography>
-        ) : (
-          <Typography color="textSecondary" variant="body1" gutterTop>
-            {types?.join(' • ')}
-          </Typography>
-        )}
-      </Spacing>
+      <Spacing container mt={1} fd="row" ai="center">
+        {horizontal ? (
+          <Spacing mr={2}>
+            <Rating>
+              <Typography color="textWhite" variant="body1">
+                4.3
+              </Typography>
+            </Rating>
 
-      <Spacing mt={1}>
-        <Flex flexDirection="row" align="center">
-          <Rating>
-            <Typography color="textWhite" variant="body2">
-              4.3
-            </Typography>
-          </Rating>
-          <Spacing mr={1}>
-            <Typography color="textSecondary" varia nt="body1">
+            <Typography color="grey" variant="body1">
               25min
             </Typography>
-          </Spacing>
-          <Spacing mr={1}>
-            <Typography color="textSecondary" varia nt="body1">
-              •
+
+            <Typography color="grey" variant="body1">
+              Free delivery
             </Typography>
           </Spacing>
-          <Typography color="textSecondary" variant="body1">
-            Free delivery
-          </Typography>
-        </Flex>
+        ) : (
+          <Spacing container fd="row" jc="center" ai="center">
+            <Typography color="grey" variant="body1">
+              4.3
+            </Typography>
+            <Spacing mr={1} ml={1}>
+              <SvgIcon icon="Star" color={theme.palette.success.main.string()} />
+            </Spacing>
+            <Typography color="grey" variant="body1">
+              200+ Ratings
+            </Typography>
+            <Spacing container fd="row" jc="center" ai="center" mr={1} ml={1}>
+              <Spacing mr={1} ml={1}>
+                <SvgIcon icon="Time" color={theme.palette.success.main.string()} />
+              </Spacing>
+              <Typography color="grey" variant="body1">
+                25 Min
+              </Typography>
+            </Spacing>
+            <Spacing container fd="row" jc="center" ai="center" mr={1} ml={1}>
+              <Spacing mr={1} ml={1}>
+                <SvgIcon icon="Currency" color={theme.palette.success.main.string()} />
+              </Spacing>
+              <Typography color="grey" variant="body1">
+                Free
+              </Typography>
+            </Spacing>
+          </Spacing>
+        )}
       </Spacing>
-    </Root>
+    </Spacing>
   )
 }
 
 ProductToolbar.propTypes = {
-  address: PropTypes.string,
-  isProductScreen: PropTypes.bool,
+  horizontal: PropTypes.bool,
   title: PropTypes.string,
-  type: PropTypes.string,
   types: PropTypes.array,
 }
 
